4f3802ffa20b8d7f76863eafddcd8cfed0a1f03b
piko <piko@8a9d4e5d-ce61-0410-bf9c-9d46fbdb99af>
2009-02-19 21:00:12 +0000

    -adding retiler -removing usage of printstacktrace simboss1 | 2009-01-20 00:35:30 +0100 (Tue, 20 Jan 2009)

--- trunk/dao/xstream/src/main/java/it/geosolutions/iengine/catalog/dao/file/xstream/XStreamFlowConfigurationDAO.java
+++ trunk/dao/xstream/src/main/java/it/geosolutions/iengine/catalog/dao/file/xstream/XStreamFlowConfigurationDAO.java
@@ -1,17 +1,23 @@
 package it.geosolutions.iengine.catalog.dao.file.xstream;
 
-import com.thoughtworks.xstream.XStream;
 import it.geosolutions.iengine.catalog.dao.FlowManagerConfigurationDAO;
 import it.geosolutions.iengine.configuration.flow.FlowConfiguration;
 import it.geosolutions.iengine.configuration.flow.file.FileBasedFlowConfiguration;
-
 import it.geosolutions.iengine.xstream.Alias;
+
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.thoughtworks.xstream.XStream;
 
 public class XStreamFlowConfigurationDAO extends XStreamDAO<FlowConfiguration> implements
         FlowManagerConfigurationDAO {
 
+	private final static Logger LOGGER=Logger.getLogger(XStreamFlowConfigurationDAO.class.toString());
+	
     public XStreamFlowConfigurationDAO(String directory) {
         super(directory);
     }
@@ -28,11 +34,12 @@ public class XStreamFlowConfigurationDAO extends XStreamDAO<FlowConfiguration> i
                 Alias.setAliases(xstream);
 
                 FileBasedFlowConfiguration obj = (FileBasedFlowConfiguration) xstream
-                        .fromXML(new FileInputStream(entityfile));
+                        .fromXML(new BufferedInputStream(new FileInputStream(entityfile)));
                 return obj;
             }
         } catch (Throwable e) {
-            e.printStackTrace();
+            if(LOGGER.isLoggable(Level.SEVERE))
+            	LOGGER.log(Level.SEVERE,e.getLocalizedMessage(),e);
         }
         return null;
     }
