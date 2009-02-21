f25e915d0324ca178d9172f25d6fbdae5f07e2f8
piko <piko@8a9d4e5d-ce61-0410-bf9c-9d46fbdb99af>
2009-02-21 17:58:15 +0000

    - adjustment project name, information , url and comments

--- dao/jibx/src/main/java/it/geosolutions/iengine/global/JIBXCatalogLoader.java
+++ dao/jibx/src/main/java/it/geosolutions/iengine/global/JIBXCatalogLoader.java
@@ -1,6 +1,6 @@
 /*
  *  GeoBatch - Open Source geospatial batch processing system
- *  http://code.google.com/p/geobatch/
+ *  http://geobatch.codehaus.org/
  *  Copyright (C) 2007-2008-2009 GeoSolutions S.A.S.
  *  http://www.geo-solutions.it
  *
@@ -64,7 +64,7 @@ public class JIBXCatalogLoader extends CatalogHolder implements ApplicationConte
     private ApplicationContext context;
 
     /**
-     * IngestionEngine data dir. This directory is used by the Ingestion Engine to store Flows
+     * IngestionEngine data dir. This directory is used by the GeoBatch to store Flows
      * configuration files.
      */
     private File dataDir;
