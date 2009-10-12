b375a9f602b5f2379c599ea5848ffb16d900bca7
francesco.izzi <francesco.izzi@8a9d4e5d-ce61-0410-bf9c-9d46fbdb99af>
2009-10-12 09:15:25 +0000

    Aligned with alias coverage afabiani patch

--- flowmanagers/ais/src/main/java/it/geosolutions/geobatch/ais/raster/AISCoverageGeoServerGenerator.java
+++ flowmanagers/ais/src/main/java/it/geosolutions/geobatch/ais/raster/AISCoverageGeoServerGenerator.java
@@ -13,14 +13,12 @@ import java.util.Map;
 
 public class AISCoverageGeoServerGenerator extends AsciiGeoServerGenerator {
 
-	protected AISCoverageGeoServerGenerator(
-			GeoServerActionConfiguration configuration) throws IOException {
+	protected AISCoverageGeoServerGenerator(GeoServerActionConfiguration configuration)
+			throws IOException {
 		super(configuration);
 	}
-
-	public void sendToGeoServer(File workingDir, FileSystemMonitorEvent event,
-			String coverageStoreId, String storeFilePrefix, String configId)
-			throws MalformedURLException, FileNotFoundException {
+	
+	public void sendToGeoServer(File workingDir, FileSystemMonitorEvent event, String coverageStoreId, String storeFilePrefix, String configId) throws MalformedURLException, FileNotFoundException {
 		// ////////////////////////////////////////////////////////////////////
 		//
 		// SENDING data to GeoServer via REST protocol.
@@ -30,27 +28,32 @@ public class AISCoverageGeoServerGenerator extends AsciiGeoServerGenerator {
 		LOGGER.info("Sending ArcGrid to GeoServer ... "
 				+ getConfiguration().getGeoserverURL());
 		Map<String, String> queryParams = new HashMap<String, String>();
-		queryParams.put("namespace", getConfiguration().getDefaultNamespace());
+		queryParams.put("namespace", getConfiguration()
+				.getDefaultNamespace());
 		queryParams.put("wmspath", getConfiguration().getWmsPath());
 		queryParams.put("style", getConfiguration().getDefaultStyle());
 
 		send(workingDir, event.getSource(), getConfiguration()
-				.getGeoserverURL(), new Long(event.getTimestamp()).toString(),
-				coverageStoreId, storeFilePrefix, getConfiguration()
-						.getStyles(), configId, getConfiguration()
-						.getDefaultStyle(), queryParams, getConfiguration()
-						.getDataTransferMethod());
+				.getGeoserverURL(), new Long(event.getTimestamp())
+				.toString(), coverageStoreId, storeFilePrefix,
+				getConfiguration().getStyles(), configId,
+				getConfiguration().getDefaultStyle(), queryParams,
+				getConfiguration().getDataTransferMethod());
 
 		LOGGER.info("Update Last" + getConfiguration().getId()
 				+ " GeoServer layer  ... "
 				+ getConfiguration().getGeoserverURL());
 
+		queryParams.put("coverageName", "Last"+getConfiguration().getId());
+		
 		send(workingDir, event.getSource(), getConfiguration()
-				.getGeoserverURL(), new Long(event.getTimestamp()).toString(),
-				"Last" + getConfiguration().getId(), storeFilePrefix,
-				getConfiguration().getStyles(), configId, getConfiguration()
-						.getDefaultStyle(), queryParams, getConfiguration()
-						.getDataTransferMethod());
+				.getGeoserverURL(), new Long(event.getTimestamp())
+				.toString(), "Last" + getConfiguration().getId(),
+				storeFilePrefix, getConfiguration().getStyles(), configId,
+				getConfiguration().getDefaultStyle(), queryParams,
+				getConfiguration().getDataTransferMethod());
 	}
+	
+	
 
 }
