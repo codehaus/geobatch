e8de8c2b9463bfdee933b6abe46f8006def81178
francesco.izzi <francesco.izzi@8a9d4e5d-ce61-0410-bf9c-9d46fbdb99af>
2009-10-13 14:52:51 +0000

    Add GeoBatchServer class for start up ftp-server with spring

--- trunk/gb-ftp-server/src/main/java/it/geosolutions/geobatch/ftp/server/GeoBatchServer.java
+++ trunk/gb-ftp-server/src/main/java/it/geosolutions/geobatch/ftp/server/GeoBatchServer.java
@@ -7,21 +7,17 @@ import it.geosolutions.geobatch.ftp.server.model.FtpUser;
 import java.util.logging.Logger;
 
 import org.apache.ftpserver.FtpServer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.InitializingBean;
 
-public class GeoBatchServer {
+public class GeoBatchServer implements InitializingBean {
 
 	private static Logger logger = Logger.getLogger(GeoBatchServer.class
 			.getName());
 
-	public static void main(String[] args) throws Throwable {
+	private DAOFtpUserHibernate ftpUserDAO;
+	private FtpServer ftpServer;
 
-		ApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext(
-				"applicationContext.xml");
-		DAOFtpUserHibernate ftpUserDAO = (DAOFtpUserHibernate) classPathXmlApplicationContext
-				.getBean("ftpUserDAO");
-
+	public void afterPropertiesSet() throws Exception {
 		FtpUser ftpUser = new FtpUser();
 		ftpUser.setUserId("giuseppe");
 		ftpUser.setUserPassword("0x,peppino,0x");
@@ -30,16 +26,61 @@ public class GeoBatchServer {
 
 		try {
 			ftpUserDAO.makePersistent(ftpUser);
-			logger.info("####################### Insert " + ftpUser.toString()
-					+ " in the db ################################");
+			logger
+					.info("################################ Insert "
+							+ ftpUser.toString()
+							+ " in the db #######################");
+			this.ftpServer.start();
 		} catch (DAOException e) {
 			// TODO Auto-generated catch block
-			logger.finest("ERROR : " + e.getMessage());
+			logger.info("ERROR : " + e);
 		}
-
-		FtpServer ftpServer = (FtpServer) classPathXmlApplicationContext
-				.getBean("server");
-		ftpServer.start();
 	}
+
+	/**
+	 * @param ftpUserDAO
+	 *            the ftpUserDAO to set
+	 */
+	public void setFtpUserDAO(DAOFtpUserHibernate ftpUserDAO) {
+		this.ftpUserDAO = ftpUserDAO;
+		logger.info("FTP SERVER SESSION FACTORY######################### " + ftpUserDAO.getSessionFactory().hashCode());
+	}
+
+	/**
+	 * @param ftpServer
+	 *            the ftpServer to set
+	 */
+	public void setFtpServer(FtpServer ftpServer) {
+		this.ftpServer = ftpServer;
+	}
+
+	// public static void main(String[] args) throws Throwable {
+	//
+	// ApplicationContext classPathXmlApplicationContext = new
+	// ClassPathXmlApplicationContext(
+	// "applicationContext.xml");
+	// DAOFtpUserHibernate ftpUserDAO = (DAOFtpUserHibernate)
+	// classPathXmlApplicationContext
+	// .getBean("ftpUserDAO");
+	//
+	// FtpUser ftpUser = new FtpUser();
+	// ftpUser.setUserId("giuseppe");
+	// ftpUser.setUserPassword("0x,peppino,0x");
+	// ftpUser.setHomeDirectory("/tmp/");
+	// ftpUser.setWritePermission(true);
+	//
+	// try {
+	// ftpUserDAO.makePersistent(ftpUser);
+	// logger.info("####################### Insert " + ftpUser.toString()
+	// + " in the db ################################");
+	// } catch (DAOException e) {
+	// // TODO Auto-generated catch block
+	// logger.finest("ERROR : " + e.getMessage());
+	// }
+	//
+	// FtpServer ftpServer = (FtpServer) classPathXmlApplicationContext
+	// .getBean("server");
+	// ftpServer.start();
+	// }
 
 }
