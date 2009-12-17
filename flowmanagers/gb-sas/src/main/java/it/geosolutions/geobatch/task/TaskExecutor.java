40c754a25ec56928e412e2ce45cc7a17d9f44031
dany111 <dany111@8a9d4e5d-ce61-0410-bf9c-9d46fbdb99af>
2009-12-17 17:24:49 +0000

    Adding TaskExecutor

---
+++ flowmanagers/gb-sas/src/main/java/it/geosolutions/geobatch/task/TaskExecutor.java
@@ -0,0 +1,87 @@
+/*
+ *  GeoBatch - Open Source geospatial batch processing system
+ *  http://geobatch.codehaus.org/
+ *  Copyright (C) 2007-2008-2009 GeoSolutions S.A.S.
+ *  http://www.geo-solutions.it
+ *
+ *  GPLv3 + Classpath exception
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package it.geosolutions.geobatch.task;
+
+import it.geosolutions.filesystemmonitor.monitor.FileSystemMonitorEvent;
+import it.geosolutions.geobatch.flow.event.action.Action;
+import it.geosolutions.geobatch.flow.event.action.BaseAction;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Queue;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.ExecTask;
+import org.apache.tools.ant.types.Environment.Variable;
+
+/**
+ * Comments here ...
+ * 
+ * @author Daniele Romagnoli, GeoSolutions
+ */
+public class TaskExecutor extends BaseAction<FileSystemMonitorEvent> implements Action<FileSystemMonitorEvent> {
+
+    private TaskExecutorConfiguration configuration;
+
+	public TaskExecutor(TaskExecutorConfiguration configuration) throws IOException {
+    	this.configuration = configuration;
+    }
+
+	public Queue<FileSystemMonitorEvent> execute(
+			Queue<FileSystemMonitorEvent> events) throws Exception {
+		
+		final Project project = new Project();
+		project.init();
+
+		final ExecTask execTask = new ExecTask();
+		execTask.setProject(project);
+		
+		List<String> variables = configuration.getVariables();
+		for (String variable: variables){
+			String keyValuePair[] = variable.split(" ");
+			Variable var = new Variable();
+			var.setKey(keyValuePair[0]);
+			var.setValue(keyValuePair[1]);
+			execTask.addEnv(var);
+		}
+		
+		execTask.setExecutable(configuration.getExecutable());
+		
+		final String errorFile = configuration.getErrorFile();
+		if (errorFile!=null && errorFile.trim().length()>0){
+			execTask.setLogError(true);
+			execTask.setError(new File(errorFile));
+			execTask.setFailonerror(true);
+		}
+			
+		Long timeOut = configuration.getTimeOut();
+		if (timeOut!=null){
+			execTask.setTimeout(timeOut);
+		}
+		execTask.createArg().setLine(configuration.getArgument());
+		execTask.execute();
+		return events;
+	}
+
+}
