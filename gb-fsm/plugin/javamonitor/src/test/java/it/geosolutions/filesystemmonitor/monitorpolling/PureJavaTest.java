c73bdd3499d5c5c88a7cd1903f0ba1ed169f97f6
simboss <simboss@8a9d4e5d-ce61-0410-bf9c-9d46fbdb99af>
2010-01-11 08:30:41 +0000

    fixing build

--- gb-fsm/plugin/javamonitor/src/test/java/it/geosolutions/filesystemmonitor/monitorpolling/PureJavaTest.java
+++ gb-fsm/plugin/javamonitor/src/test/java/it/geosolutions/filesystemmonitor/monitorpolling/PureJavaTest.java
@@ -35,7 +35,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestExecutionListeners({})
-@ContextConfiguration(locations={"/applicationContext.xml", "/TestDummy-context.xml"})
+@ContextConfiguration(locations={"/applicationContext.xml"})
 public class PureJavaTest extends AbstractJUnit4SpringContextTests {
 
 
